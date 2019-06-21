bfbbd88dd40e24e2a37243b2e28d1cdc3841dbec
Christian Treffs <ctreffs@gmail.com>
2019-06-21 15:38:38 +0200

    Add tests

--- Tests/AssimpTests/AssimpTests.swift
+++ Tests/AssimpTests/AssimpTests.swift
@@ -7,42 +7,37 @@ final class AssimpTests: XCTestCase {
     static var allTests = [
         ("testFailingInitializer", testFailingInitializer)
     ]
-    var standfordBunnyObjFile: URL!
-
-    override func setUp() {
-        super.setUp()
-        let fm = FileManager.default
-
-        var tmpDir: URL!
-        if #available(OSX 10.12, *) {
-            tmpDir = fm.temporaryDirectory
-        } else {
-            fatalError()
-        }
-
-        standfordBunnyObjFile = tmpDir.appendingPathComponent("standfordBunny.obj")
-
-        if !fm.fileExists(atPath: standfordBunnyObjFile.path) {
-            let data = try! Data.init(contentsOf: Resources.stanfordBunnyObj)
-            try! data.write(to: standfordBunnyObjFile)
-        }
-
-    }
-
-    override func tearDown() {
-        super.tearDown()
-    }
 
     func testFailingInitializer() {
         XCTAssertThrowsError(try AiScene(file: "<no useful path>"))
     }
 
-    func testLoadAiSceneObj() {
+    func testLoadAiSceneDAE() {
+        
+        let fileURL = try! Resource.load(.duck_dae)
+        
         var scene: AiScene!
-        XCTAssertNoThrow(scene = try AiScene(file: standfordBunnyObjFile.path))
+        XCTAssertNoThrow(scene = try AiScene(file: fileURL.path))
         XCTAssertEqual(scene.numMeshes, 1)
-
-        XCTAssertEqual(scene.meshes[0].name, "defaultobject") // assimp default naming
+        XCTAssertEqual(scene.numTextures, 0)
+        
+        XCTAssertEqual(scene.meshes[0].name, "LOD3spShape")
+        XCTAssertEqual(scene.meshes[0].numVertices, 8500)
+        XCTAssertEqual(scene.meshes[0].numFaces, 2144)
+    }
+    
+    func testLoadAiSceneObj() {
+        
+        let fileURL = try! Resource.load(.box_obj)
+        
+        var scene: AiScene!
+        XCTAssertNoThrow(scene = try AiScene(file: fileURL.path))
+        XCTAssertEqual(scene.numMeshes, 1)
+        XCTAssertEqual(scene.numTextures, 0)
+        
+        XCTAssertEqual(scene.meshes[0].name, "1")
+        XCTAssertEqual(scene.meshes[0].numVertices, 8 * 3)
+        XCTAssertEqual(scene.meshes[0].numFaces, 6)
     }
 
 }
--- Tests/AssimpTests/Resources.swift
+++ Tests/AssimpTests/Resources.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 
-enum Resources {
-    static let stanfordBunnyObj: URL = URL(string: "https://graphics.stanford.edu/~mdfisher/Data/Meshes/bunny.obj")!
+enum Resource: String {
+    case bunny_obj = "https://graphics.stanford.edu/~mdfisher/Data/Meshes/bunny.obj"
+    
+    case duck_dae = "https://raw.githubusercontent.com/assimp/assimp/master/test/models/Collada/duck.dae"
+    case box_obj = "https://raw.githubusercontent.com/assimp/assimp/master/test/models/OBJ/box.obj"
+    
+    private static let fm = FileManager.default
+    
+    enum Error: Swift.Error {
+        case invalidURL(String)
+    }
+    
+    static func tmpDir() -> URL {
+        let tmpDir: URL
+        if #available(OSX 10.12, *) {
+            tmpDir = fm.temporaryDirectory
+        } else {
+            fatalError("needs temp dir")
+        }
+        return tmpDir
+    }
+    
+    static func load(_ resource: Resource) throws -> URL {
+        guard let remoteURL: URL = URL(string: resource.rawValue) else {
+            throw Error.invalidURL(resource.rawValue)
+        }
+        
+        let localFile: URL = tmpDir().appendingPathComponent(remoteURL.lastPathComponent)
+        if !fm.fileExists(atPath: localFile.path) {
+            let data = try Data(contentsOf: remoteURL)
+            try data.write(to: localFile, options: .atomicWrite)
+        }
+        
+        return localFile
+    }
 }
