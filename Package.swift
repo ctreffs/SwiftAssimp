e6dd0b1b8f666b4ef3426f3c3fd25c3df071f0f3
Christian Treffs <ctreffs@gmail.com>
2022-01-27 20:51:12 +0100

    Update to Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -14,14 +14,10 @@ let package = Package(
         .testTarget(name: "AssimpTests", dependencies: ["Assimp"]),
         .systemLibrary(name: "CAssimp",
                        path: "Sources/CAssimp",
-            // we are unable to use pkg config here since assimp has an issue in it's .pc file. See: <https://github.com/assimp/assimp/issues/2804>
-            // we could use a custom PKG_CONFIG_PATH to add a custom .pc file (https://stackoverflow.com/a/47107882/6043526) but this is a big hassle.
-            // just copy the correct file before compiling! See README.md for instructions.
-            pkgConfig: "assimp",
-            providers: [
-                .brew(["assimp"]),
-                .apt(["libassimp-dev"]),
-                
-        ])
+                       pkgConfig: "assimp",
+                       providers: [
+                        .brew(["assimp"]),
+                        .apt(["libassimp-dev"]),
+                       ])
     ]
 )
